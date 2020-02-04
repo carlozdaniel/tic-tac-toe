b99255308232fc4ef02d9110d30114aba79dd76e
Carlos Daniel H.R <clear>
2020-02-04 11:51:04 -0600

    create a new main class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,154 +1,26 @@
-
-class Gato 
-    
-  def welcome_message()
-    print 'dame el tamano del board: '
-    board_size = gets.chomp
-    @board_size = board_size.to_i 
-    $total_positions = @board_size * @board_size.to_i
-    puts "tu board sera de #{@board_size} X #{@board_size}"
-    @board = []
-    Array.new
-  end
-   
-  def Insert_data_into_dashboard()
-    c = 1
-    @board_size.times do
-      @column = []
-      @board_size.times do   
-        @column.push c
-        c += 1
-      end
-      @board.push @column
-    end
-  end
-
-  def print_board()
-    @board.each do |column|
-      column.each do |card|
-        print card
-    
-        print '  |  '
-      end
-      puts '' 
-
-   end 
-  end
-  
-  def player_x()
-    print "1 dato de la cordenada"
-    f=gets.chomp.to_i
-    print "2 dato de la cordenada"
-    c=gets.chomp.to_i
-    @board[f][c] ="x"    
-  end 
-
-  def player_o
-    print "1 dato de la cordenada"
-    f=gets.chomp.to_i
-    print "2 dato de la cordenada"
-    c=gets.chomp.to_i
-    @board[f][c] = "o"   
-  end
- 
-
-  def validate_row()
-    @board_size.times do |x|
-      row = []
-      @board_size.times do |c|
-        row << @board[x][c]
-
-      end
-      if row.uniq.length == 1
-        puts "ganador en linea"
-       exit
-        gets
-        return   
-      end
-    end
-  false
-  end
-
-  def validate_column()
-    @board_size.times do |c|
-      row = []
-          @board_size.times do |x|
-            row << @board[x][c]
-            #uniq.length
-          end
-      if row.uniq.length == 1
-        puts "ganador en columna"
-       exit
-       return  
-      end
-    end
-    false
-  end
-
-  def diagonal_inverted
-      diagonal_inverted = []
-      @board_size.times do |c|
-      diagonal_inverted << @board[c][c]     
-    end
-        if diagonal_inverted.uniq.length == 1
-          puts "ganador en diagonal invertida"
-         exit
-          gets
-          return
-        end 
-        
-    end
-
-    
-    def diagonal 
-      board_size_menos_1 = @board_size - 1
-  sum = [] 
-      @board_size.times do |x|
-        
-            @board_size.times do |c|
-              if (x+c) == board_size_menos_1
-               sum << @board[x][c]
-              end          
-     end
-
-   end
-   if sum.uniq.length == 1
-    puts "ganador en diagonal"
-   exit
-    gets
-    
-  end 
-
-  end
-  end
-
-board = Gato.new()
+require_relative 'board'
+board = Tic_tac_toe.new
 board.welcome_message
-board.Insert_data_into_dashboard
+board.insert_data_into_dashboard
 board.print_board
-
-
+par = 0
 $total_positions.times do |i|
-    
-  if (i % 2) == 0
-      puts "jugador O"
-      board.player_o
-      board.print_board
-      board.validate_row
-      board.validate_column
-      board.diagonal_inverted
-      board.diagonal
-      
-    else
-      puts "jugador X"
-      board.player_x
-      board.print_board
-      board.validate_row
-      board.validate_column
-      board.diagonal_inverted
-      board.diagonal
+  if (i % 2) == par
+    puts "\e[1;31m jugador O \e[m"
+    board.player_o
+    board.print_board
+    board.validate_row
+    board.validate_column
+    board.diagonal_inverted
+    board.diagonal
+  else
+  puts "\e[1;34m jugador X \e[m"
+  board.player_x
+  board.print_board
+  board.validate_row
+  board.validate_column
+  board.diagonal_inverted
+  board.diagonal
   end
-  
 end
-puts "enpate"
-
+p 'enpate'
